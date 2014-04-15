91927beb5f4b4bcd3b57ae99f295cec35a122c40
Vlad Meh <vladmeh@gmail.com>
2014-04-15 16:33:04 +0400

    del repo protected/runtime

--- protected/runtime/views/ebb8ccbc/column1.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'Z:\home\dendor-pl.loc\www\protected\extensions\phamlp\vendors\phamlp\haml\HamlHelpers.php';
-?><?php echo $this->beginContent('//layouts/main'); ?>
-
-<div id="content">
-<?php echo $content; ?>
-
-</div><?php echo $this->endContent(); ?>
-
