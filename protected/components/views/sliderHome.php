019d54cedaf31761d2039d72ca5a48cac5d296ee
Vlad Meh <vladmeh@gmail.com>
2014-04-23 16:12:48 +0400

    create widget slide

---
+++ protected/components/views/sliderHome.php
@@ -0,0 +1,9 @@
+<?php
+  foreach ($slides as $slide) {
+    /*echo CHtml::tag('div', array('class'=>'slider-item', 'data-slidr' => $slide->data_slidr),false, false);
+    echo CHtml::tag('div', array('class'=>'item-content'), false, false);
+    echo CHtml::tag('p',array(),CHtml::encode($slide->text));
+    echo CHtml::closeTag('div');
+    echo CHtml::closeTag('div');*/
+    echo 'Hellow Word';
+  }
