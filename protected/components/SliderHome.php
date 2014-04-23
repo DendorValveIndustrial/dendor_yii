e1ce8afa2492bfba9b015154381d3c6ef1e5803e
vladmeh home <vladmeh@gmail.com>
2014-04-24 01:21:44 +0400

    create vidget SliderHome

--- protected/components/SliderHome.php
+++ protected/components/SliderHome.php
@@ -1,5 +1,12 @@
 <?php
 class SliderHome extends CWidget {
+  public function init(){
+    $baseUrl = Yii::app()->baseUrl;
+    $cs=Yii::app()->clientScript;
+    $cs->registerScriptFile($baseUrl.'/js/slidr/slidr.min.js', CClientScript::POS_END);
+    $cs->registerScriptFile($baseUrl.'/js/slidr/slidr-example.js', CClientScript::POS_END);
+  }
+
   public function run(){
     $slides = Slider::model()->findAll();
     $this->render('sliderHome',array('slides'=>$slides));
--- protected/components/views/sliderHome.php
+++ protected/components/views/sliderHome.php
@@ -1,9 +1,8 @@
 <?php
   foreach ($slides as $slide) {
-    /*echo CHtml::tag('div', array('class'=>'slider-item', 'data-slidr' => $slide->data_slidr),false, false);
+    echo CHtml::tag('div', array('class'=>'slider-item', 'data-slidr' => $slide->data_slidr),false, false);
     echo CHtml::tag('div', array('class'=>'item-content'), false, false);
     echo CHtml::tag('p',array(),CHtml::encode($slide->text));
     echo CHtml::closeTag('div');
-    echo CHtml::closeTag('div');*/
-    echo 'Hellow Word';
+    echo CHtml::closeTag('div');
   }
