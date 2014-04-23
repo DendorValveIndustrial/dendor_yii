42966c76803e26df962b3c11fec2547aba6fde0c
Vlad Meh <vladmeh@gmail.com>
2014-04-23 14:46:36 +0400

    create slider model and CRUID

--- protected/migrations/m140423_091258_create_table_slider.php
+++ protected/migrations/m140423_091258_create_table_slider.php
@@ -13,6 +13,12 @@ class m140423_091258_create_table_slider extends CDbMigration
 			'link' => 'string',
 			'active' => 'boolean NOT NULL DEFAULT 1',
 		));
+
+		$this->insert('slider', array(
+			'data_slidr' => 'one',
+			'text' => 'Oferujemy przepustnice zwrotne wyprodukowane ze stali, żeliwa sferoidalnego, PVC. Kołnierzowe, bezkołnierzowe, do wspawania, mimośrodowe w zakresie średnic DN – od 32 do 2000.',
+			'link' => '/about/',
+		));
 	}
 
 	public function down()
---
+++ protected/views/slider/_form.php
@@ -0,0 +1,64 @@
+<?php
+/* @var $this SliderController */
+/* @var $model Slider */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'slider-form',
+	// Please note: When you enable ajax validation, make sure the corresponding
+	// controller action is handling ajax validation correctly.
+	// There is a call to performAjaxValidation() commented in generated controller code.
+	// See class documentation of CActiveForm for details on this.
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'data_slidr'); ?>
+		<?php echo $form->textField($model,'data_slidr',array('size'=>60,'maxlength'=>255)); ?>
+		<?php echo $form->error($model,'data_slidr'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'name'); ?>
+		<?php echo $form->textField($model,'name',array('size'=>60,'maxlength'=>255)); ?>
+		<?php echo $form->error($model,'name'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'text'); ?>
+		<?php echo $form->textArea($model,'text',array('rows'=>6, 'cols'=>50)); ?>
+		<?php echo $form->error($model,'text'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'img'); ?>
+		<?php echo $form->textField($model,'img',array('size'=>60,'maxlength'=>255)); ?>
+		<?php echo $form->error($model,'img'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'link'); ?>
+		<?php echo $form->textField($model,'link',array('size'=>60,'maxlength'=>255)); ?>
+		<?php echo $form->error($model,'link'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'active'); ?>
+		<?php echo $form->textField($model,'active'); ?>
+		<?php echo $form->error($model,'active'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
