53767de655a4a3ed13d4e486cacdf54b2feae032
Vlad Meh <vladmeh@gmail.com>
2014-04-30 16:48:06 +0400

    vladmeh dendor 30.04

--- protected/views/site/search.php
+++ protected/views/site/search.php
@@ -44,7 +44,6 @@
     var gcse = document.createElement('script');
       gcse.type = 'text/javascript';
       gcse.async = true;
-      gcse.qwery = 'dendor';
       gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//www.google.com/cse/cse.js?cx=' + cx;
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(gcse, s); })();",
