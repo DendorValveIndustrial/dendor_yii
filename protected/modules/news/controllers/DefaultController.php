7649772e64a97367347fa781cca08486d7b779c0
vladmeh <vladmeh@gmail.com>
2015-01-27 02:31:11 +0300

    vladmeh home

--- protected/modules/news/controllers/DefaultController.php
+++ protected/modules/news/controllers/DefaultController.php
@@ -6,4 +6,13 @@ class DefaultController extends Controller
 	{
 		$this->render('index');
 	}
+
+  /**
+   * Override default method to return category full_url without encoded slash.
+   * TODO: Find right solution for '/' in url params.
+   */
+  public function createUrl($route,$params=array(),$ampersand='&')
+  {
+    return urldecode(parent::createUrl($route,$params,$ampersand));
+  }
 }
