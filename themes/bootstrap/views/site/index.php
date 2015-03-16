88a82fb04957125ced57735086bb46cced35383e
Vlad Meh <vladmeh@gmail.com>
2015-03-16 17:03:35 +0300

    vladmeh dendor

--- themes/bootstrap/views/site/index.php
+++ themes/bootstrap/views/site/index.php
@@ -27,35 +27,33 @@
       </div>
     </div>
     <div class='line line-dotted'></div>
-    <div class='row-fluid'>
-      <div class='span6 home-news'>
-        <?php echo CHtml::tag('h2',array('class'=>'h1'),Yii::t('app','news')) ?>
-        <p>
-          <?php
-            $latest_news = News::model()->find();
-            echo CHtml::encode($latest_news->short_description);
-          ?>
-        </p>
-        <p class='text-right'>
-          <?php echo CHtml::link(Yii::t('app','read_more'),$latest_news->viewUrl); ?>
-        </p>
-      </div>
-      <div class='span6 home-news-box'>
-        <ul class='thumbnails'>
-          <li class='span5 offset1'>
-            <div class='thumbnail text-center'>
-              <a href="#"><img alt="lorem_image" height="180" src="http://placehold.it/180x180" width="180" /></a>
-              <h5>Catalogue</h5>
-            </div>
-          </li>
-          <li class='span5 offset1'>
-            <div class='thumbnail text-center'>
-              <a href="#"><img alt="lorem_image" height="180" src="http://placehold.it/180x180" width="180" /></a>
-              <h5>Сertificate</h5>
-            </div>
-          </li>
-        </ul>
-      </div>
+    <div class='home-news'>
+      <?php echo CHtml::tag('h2',array('class'=>'h1'),Yii::t('app','news')) ?>
+      <ul class="thumbnails">
+        <li class="span6">
+            <p>
+              <?php
+                $latest_news = News::model()->find();
+                echo CHtml::encode($latest_news->short_description);
+              ?>
+            </p>
+            <p class='text-right'>
+              <?php echo CHtml::link(Yii::t('app','read_more'),$latest_news->viewUrl); ?>
+            </p>
+        </li>
+        <li class="span3">
+          <a href="#" class="thumbnail span10 offset2">
+            <img alt="lorem_image" src="http://placehold.it/180x120" style="width: 180px; height: 120px;"/>
+            <div class="caption text-center">Catalogue</div>
+          </a>
+        </li>
+        <li class="span3">
+          <a href="#" class="thumbnail span10 offset2">
+            <img alt="lorem_image" src="http://placehold.it/180x120" style="width: 180px; height: 120px;"/>
+            <div class="caption text-center">Сertificate</div>
+          </a>
+        </li>
+      </ul>
     </div>
   </div>
 </div>
